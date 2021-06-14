e195fe85001ff8db63c05b0b0ec6029edd2104ce
Vinícius Gonçalves <82480698+VinniGon@users.noreply.github.com>
2021-06-14 18:38:18 -0300

    upload
    Projeto de Empreendedorismo

---
+++ js/app.js
@@ -0,0 +1,58 @@
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Scroll suave para link interno
+$('.menu-nav a[href^="#"]').on('touchstart click', function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.menu').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// Scroll suave para o topo
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+});
+
+// Mudar para active o menu de acordo com a área
+$('section').each(function(){
+	var height = $(this).height(),
+			offsetTop = $(this).offset().top,
+			menuHeight = $('.menu').innerHeight(),
+			id = $(this).attr('id'),
+			$itemMenu = $('a[href="#' + id + '"]');
+	
+	$(window).scroll(debounce(function(){
+		var scrollTop = $(window).scrollTop();
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
+	}, 200));
+});
+
+// Botão do menu mobile
+$('.mobile-btn').click(function(){
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
+});
